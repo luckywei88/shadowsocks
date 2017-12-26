425f527714c9779a5f7baf4c06c9475d85606e22
luckywei88 <1010224593@qq.com>
2017-12-26 13:00:29 +0800

    i

--- install_ros.sh
+++ install_ros.sh
@@ -1,20 +1,88 @@
 #!/bin/bash
 
 
+Lib=/home/lucky/MyLib
+Origin=/home/lucky/MyLib/OriginLib
+work=/home/lucky/workspace/
+work_src=/home/lucky/workspace/src
 
-sudo apt-get install git
+giturls=(
+	git@github.com:luckywei88/MY_DARKNET.git
+	git@github.com:luckywei88/MY_DBoW2.git
+	git@github.com:luckywei88/MY_G2O.git
+	git@github.com:luckywei88/MY_OCTOMAP.git
+	git@github.com:luckywei88/shadowsocks.git
+	git@github.com:luckywei88/MY_LAUNCH.git
+	git@github.com:luckywei88/MY_SHELL.git
+	git@github.com:luckywei88/MY_HUMANOID.git	
+	git@github.com:luckywei88/my_ros_octomap.git
+	git@github.com:luckywei88/MY_ORB_SLAM.git
+)
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+names=(
+	darknet
+	DBoW2
+	g2o
+	octomap
+	shadowsocks
+	launch
+	shell
+	humanoid
+	octomap
+	orb_slam	
+)
 
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+httpurls=(
+	https://github.com/stevenlovegrove/Pangolin
+	https://github.com/raulmur/ORB_SLAM2
+	https://github.com/OctoMap/octomap	
+)
 
-sudo apt-get update
+get_git_name()
+{
+	tmp=$1
+	tmp=${tmp##*/}
+	tmp=${tmp%.*}
+	echo $tmp
+}
 
-sudo apt-get install ros-indigo-desktop-full
-sudo apt-get install ros-indigo-map-server
-sudo apt-get install ros-indigo-sbpl
+get_http_name()
+{
+	tmp=$1
+	tmp=${tmp##*/}
+	echo $tmp
+}
 
-cmake_install()
+
+ros_install()
+{
+	sudo apt-get update
+	sudo apt-get install libglew-dev
+	sudo apt-get install git
+	git config --global user.name "lucky88"
+	git config --global user.email "1010442593@qq.com"	
+
+	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+	sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+	sudo apt-get update
+
+	sudo apt-get install ros-indigo-desktop-full
+	sudo apt-get install ros-indigo-map-server
+	sudo apt-get install ros-indigo-sbpl
+}
+
+git_download()
+{
+	if [ ! -d $3 ]; then
+		git clone $1
+		mv $2 $3
+		cd $3
+		git init
+		cd ..
+	fi
+}
+
+cmaking()
 {
     cd $1
     if [ -d build ]; then
@@ -27,90 +95,91 @@ cmake_install()
     cd ../../
 }
 
-make_install()
+making()
 {
     cd $1
     make -j
     cd ../
 }
 
-install()
+
+building()
+{
+	case $1 in
+		darknet)
+		making $1
+		;;
+		ORB_SLAM2)
+		cd ORB_SLAM2
+		sh build.sh
+		cd ..
+		;;
+		shadowsocks)
+		;;
+		*)
+		cmaking $1
+		;;
+	esac	
+}
+
+make_install()
 {
     cd $1
     sudo make install
     cd ..
 }
 
-cd ~
-mkdir MyLib
-cd MyLib
+cd_MyDir()
+{
+	if [ ! -d $1 ]; then
+		mkdir -p $1
+	fi
+	cd $1
+}
 
-if [ ! -d DBoW2 ]; then
-	git clone https://github.com/luckywei88/MY_DBoW2
-	mv MY_DBoW2 DBoW2
-	cmake_install DBoW2
-fi
-
-
-if [ ! -d octomap ]; then
-	git clone https://github.com/luckywei88/MY_OCTOMAP
-	mv MY_OCTOMAP  octomap
-	cmake_install octomap
-fi
-
-if [ ! -d darknet ]; then
-	git clone https://github.com/luckywei88/MY_DARKNET
-	mv MY_DARKNET darknet
-	make_install darknet
-fi
-
-if [ ! -d Pangolin ]; then
-	git clone https://github.com/stevenlovegrove/Pangolin
-	sudo apt-get install libglew-dev
-	cmake_install Pangolin
-	install Pangolin
-fi
-
-mkdir OriginLib
-cd OriginLib
-
-if [ ! -d ORB_SLAM2 ]; then
-	git clone https://github.com/raulmur/ORB_SLAM2
-	cd ORB_SLAM2
-	sh build.sh
-	cd ..
-fi
-
-if [ ! -d octomap ]; then
-	git clone https://github.com/OctoMap/octomap
-	cmake_install octomap
-fi
-
-cd ..
-if [ ! -d g2o ]; then
-	cd OriginLib/ORB_SLAM2/Thirdparty
-	cp -r g2o .
-	cmake_install g2o
-fi
-
-
-source /opt/ros/indigo/setup.bash
-cd ~
-if [ ! -d workspace ]; then
-	mkdir -p workspace/src
-	cd workspace
-	git clone https://github.com/luckywei88/MY_SHELL
-	mv MY_SHELL shell
-	git clone https://github.com/luckywei88/MY_LAUNCH
-	mv MY_LAUNCH launch
-
-	cd src
-	git clone https://github.com/luckywei88/MY_ORB_SLAM
-	mv MY_ORB_SLAM orb_slam
-	git clone https://github.com/luckywei88/my_ros_octomap
-	mv my_ros_octomap octomap
-	git clone https://github.com/luckywei88/MY_HUMANOID
-	mv MY_HUMANOID humanoid
+install()
+{
+	#ros_install
+	
+	
+	for (( i=0;i<${#httpurls[@]};i++));
+	do
+		url=${httpurls[$i]}
+		name=$(get_http_name $url)
+		echo $i" "$url" "$name
+		if [ $i -le 0 ]; then
+			cd_MyDir $Lib
+			git_download $url $name $name
+			building $name
+			make_install $name 
+		else 
+			cd_MyDir $Origin
+			building $name
+			git_download $url $name $name 
+		fi
+	done
+	
+	for (( i=0;i<${#giturls[@]};i++));
+	do
+		url=${giturls[$i]}
+		first=$(get_git_name $url)
+		name=${names[$i]}
+		echo $i" "$url" "$first" "$name
+		if [ $i -le 4 ]; then
+			cd_MyDir $Lib
+			git_download $url $first $name
+			building $name
+		elif [ $i -le 6 ]; then
+			cd_MyDir $work
+			git_download $url $first $name
+		else
+			cd_MyDir $work_src
+			git_download $url $first $name
+		fi
+	done
+	cd_MyDir $work_src
 	catkin_init_workspace 
-fi
+	echo	
+}
 
+install 2>&1 | tee $0.log 
