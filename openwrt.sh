7e4ec885364335ef410a1ff9862a37142a4091f0
luckywei88 <1010224593@qq.com>
2018-01-02 13:00:36 +0800

    upload

--- openwrt.sh
+++ openwrt.sh
@@ -1,19 +1,45 @@
 #!/bin/bash
 
+workdir=/home/lucky/openwrt_all/
+out_luci=luci/
+out_net=packages/net/
+out_lib=packagse/libs/
+in_luci=openwrt/package/luci
+in_net=openwrt/package/network
+in_lib=openwrt/package/libs
 
-luci=package/luci/
-network=package/network/
+required_net=
+(
+	openwrt-chinadns
+	openwrt-dns-forwarder
+	shadowsocks-libev
+)
+
+required_libs=
+(
+	c-ares
+	libsodium
+	libev
+	libpcre	
+)
+
+required_luci=
+(
+	openwrt-dist-luci
+	luci-app-shadowsocks-libev
+)
+
 
 download()
 {
 	git clone https://github.com/openwrt/openwrt.git
 	git clone https://github.com/openwrt/luci.git
 	git clone https://https://github.com/openwrt/packages.git
-	pushd $network
+	pushd $out_net
 	git clone https://github.com/aa65535/openwrt-chinadns.git
 	git clone https://github.com/aa65535/openwrt-dns-forwarder.git
 	popd
-	pushd $luci
-	git clone https://github.com/techotaku/luci-app-chinadns.git
+	pushd $out_luci
+	git clone https://github.com/aa65535/openwrt-dist-luci.git
 	popd
 }
